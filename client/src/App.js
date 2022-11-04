ae04b06d11a01bf330fac02d41f118eefe1b3476
JosephW143 <josephwright1436@gmail.com>
2022-11-03 23:04:20 -0400

    snap

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -21,8 +22,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -46,13 +57,13 @@ function App() {
                 path="/signup" 
                 element={<Signup />} 
               />
-              <Route path="/profile">
-                <Route path=":username" element={<Profile />} />
-                <Route path="" element={<Profile />} />
-              </Route>
-              <Route
-                path="/thought/:id"
-                element={<SingleThought />}
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
               />
               <Route 
                 path="*" 
